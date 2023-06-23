ca0934c3d0002257bbae714c819a2cfa47db854d
ParisaDarkhal <p.darkhal@gmail.com>
2023-06-23 12:21:33 -0400

    model product created

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,3 +1,37 @@
 const { Schema, model } = require("mongoose");
 
-const 
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+      maxlength: 200,
+    },
+    price: {
+      type: Schema.Types.Decimal128,
+      required: true,
+    },
+    image: {
+      type: URL,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+    },
+
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  { collection: "users" }
+);
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
