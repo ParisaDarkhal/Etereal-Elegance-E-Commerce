74e773747b9a2030fc32cec9b7064e9317dd3a07
ParisaDarkhal <p.darkhal@gmail.com>
2023-06-22 23:12:15 -0400

    server side initialization

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server");
+const path = require("path");
+
+// mongoose connector
+const db = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Apollo Server setup
+const { typeDefs, resolvers } = require("./schemas");
+const server = new ApolloServer({ typeDefs, resolvers });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// client
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client"));
+});
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log("Server running on PORT 3001!");
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+startApolloServer();
